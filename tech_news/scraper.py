baf464550fd4dc7dcd764331ff6db1ad9fd7c188
Renan Fernandes <renzinestuods@gmail.com>
2023-04-13 14:29:28 -0300

    :sparkles: feat: Cria a função fetch

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,17 @@
+import time
+import requests
+
+
 # Requisito 1
 def fetch(url):
     """Seu código deve vir aqui"""
+    HEADERS = {"user-agent": "Fake user-agent"}
+    time.sleep(1)
+    try:
+        response = requests.get(url, headers=HEADERS, timeout=3)
+        return response.text if response.status_code == 200 else None
+    except requests.ReadTimeout:
+        return None
 
 
 # Requisito 2
@@ -21,6 +32,3 @@ def scrape_news(html_content):
 # Requisito 5
 def get_tech_news(amount):
     """Seu código deve vir aqui"""
-
-
-# init
