bae5ec09c12bee0073a95565e52f88de4d48e7a8
Renan Fernandes <renzinestuods@gmail.com>
2023-04-14 18:17:12 -0300

    :sparkles: feat: Implementa a função top_5_categories, que retorna as categorias mais populares

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,17 @@
 # Requisito 10
+from tech_news.database import db
+
+
 def top_5_categories():
     """Seu código deve vir aqui"""
+    pepelines = [
+        {
+            "$group": {
+                "_id": "$category",
+                "count": {"$sum": 1},
+            }
+        },
+        {"$sort": {"count": -1, "_id": 1}},
+        {"$limit": 5},
+    ]
+    return [item["_id"] for item in db.news.aggregate(pepelines)]
