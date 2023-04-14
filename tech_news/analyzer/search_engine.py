9cc89f2428aeab6b698be624a889cdf7bc594170
Renan Fernandes <renzinestuods@gmail.com>
2023-04-14 16:34:13 -0300

    :sparkles: feat: Implementa a função search_by_category

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -34,3 +34,9 @@ def search_by_date(date):
 # Requisito 9
 def search_by_category(category):
     """Seu código deve vir aqui"""
+    return [
+        (news["title"], news["url"])
+        for news in search_news(
+            {"category": {"$regex": f"{category}", "$options": "i"}}
+        )
+    ]
